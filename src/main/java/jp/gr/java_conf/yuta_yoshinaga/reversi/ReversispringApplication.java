438438419acdcb853becd71da5ec28c67b8fc06e
吉永　祐太 <yuta.yoshinaga@gmail.com>
2020-02-22 17:30:39 +0900

    first commit

---
+++ src/main/java/jp/gr/java_conf/yuta_yoshinaga/reversi/ReversispringApplication.java
@@ -0,0 +1,13 @@
+package jp.gr.java_conf.yuta_yoshinaga.reversi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReversispringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReversispringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/gr/java_conf/yuta_yoshinaga/reversi/ReversispringApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.gr.java_conf.yuta_yoshinaga.reversi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReversispringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
